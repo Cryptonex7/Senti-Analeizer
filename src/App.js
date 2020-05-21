d6b93c51e1b48b756baaf2f7609883893926b91c
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-05-21 21:51:27 +0530

    app init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import logo from "./assets/logo.svg";
+import "./App.css";
+import { Divider } from "@material-ui/core";
 
 function App() {
+  const [text, setText] = useState();
+  const exampleOutput = text ? "Positive" : "Enter Text to Analyse Sentiment";
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Senti-Analeizer!</h1>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="container">
+          <TextField
+            fullWidth
+            variant="outlined"
+            onChange={(e) => setText(e.target.value)}
+            label="Enter Sentiment"
+            className="input"
+          />
+
+          <br />
+          <br />
+          <Divider />
+          <br />
+
+          <div className="output">{exampleOutput}</div>
+        </div>
       </header>
     </div>
   );
